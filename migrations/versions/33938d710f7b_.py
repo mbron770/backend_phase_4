30b91e604b947c2db7b49034e63400d7b86b30e9
mbron770 <mbronfin.tsd@gmail.com>
2023-08-11 19:42:31 -0400

    db populate

---
+++ migrations/versions/33938d710f7b_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 33938d710f7b
+Revises: 
+Create Date: 2023-08-11 19:40:24.849606
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '33938d710f7b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
